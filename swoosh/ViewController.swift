8b84e7e6ae85ea935a25ff3088e78ad902a59ab3
murarivarma <murarivarma@hotmail.com>
2017-11-02 12:25:43 +0530

    worked with frames of imageviews

--- swoosh/ViewController.swift
+++ swoosh/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var bgImg: UIImageView!
+    @IBOutlet weak var swooshImg: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        swooshImg.frame = CGRect(x: view.frame.size.width/2 - swooshImg.frame.size.width/2, y: 50, width: swooshImg.frame.size.width, height: swooshImg.frame.size.height);
+        
+        bgImg.frame = view.frame;
     }
 
     override func didReceiveMemoryWarning() {
